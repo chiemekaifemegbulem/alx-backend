c6c3fd46fbf2a3c6b4981932fb85641383243506
chiemekaifemegbulem <samuelifemegbulem@my.uopeople.edu>
2023-11-29 18:29:17 +0000

    Node Redis client publisher and subscriber

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -2,6 +2,7 @@
 import { createClient } from 'redis';
 
 const client = createClient();
+const EXIT_MSG = 'KILL_SERVER';
 
 client.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.toString());
@@ -9,4 +10,14 @@ client.on('error', (err) => {
 
 client.on('connect', () => {
   console.log('Redis client connected to the server');
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === EXIT_MSG) {
+    client.unsubscribe();
+    client.quit();
+  }
 });
